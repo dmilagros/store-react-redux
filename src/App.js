9e0d733d838e68db2afa7a3b91f869d689cea769
dmilagros <dmilagrosvelasquez@gmail.com>
2021-08-22 16:41:09 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
+import ProductListing from './components/ProductListing';
+import ProductDetail from './components/ProductDetail';
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
