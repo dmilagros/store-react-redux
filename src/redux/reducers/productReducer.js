21b5a2fe95147c15a8f410125471a9844337fcb6
dmilagros <dmilagrosvelasquez@gmail.com>
2021-08-22 17:36:14 -0500

    list all products - axios

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,17 +1,13 @@
 import { actionTypes } from "../constants/actionTypes";
 
 const initialState = {
-  products: [{
-    id: 1,
-    title: "T-shirt",
-    category: "programmer"
-  }]
+  products: []
 }
 // -------- 3 REDUCERS ----------
 export const productReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case actionTypes.SET_PRODUCTS:
-      return state;
+      return { ...state, products: payload };
     default:
       return state;
   }
