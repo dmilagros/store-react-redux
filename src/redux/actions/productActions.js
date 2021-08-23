dac177972ffb815d3c2e52bc310574e4ac5f77c1
dmilagros <dmilagrosvelasquez@gmail.com>
2021-08-22 20:05:32 -0500

    redux thunk and fetch products

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,4 +1,15 @@
+import axios from "axios";
+import fakeStoreApi from "../../apis/fakeStoreApi";
 import { actionTypes } from "../constants/actionTypes"
+
+export const fetchProducts = () => async (dispatch) => {
+  //return async function (dispatch, getState) {
+  const response = await fakeStoreApi.get("/products");
+  dispatch({
+    type: actionTypes.FETCH_PRODUCTS,
+    payload: response.data
+  });
+}
 
 export const setProducts = (products) => {
   // -------- 2 ACTIONS ----------
